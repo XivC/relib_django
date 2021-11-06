8a468bbb021e22c215c3eb02c2bceb3e1dcd6394
Artem Dybov P3213 <xivxiv@niuitmo.ru>
2021-11-06 08:57:44 +0300

    Добавил комментарии (Подробно про алгоритм в documentation/)

--- api/main.py
+++ api/main.py
@@ -3,11 +3,15 @@ import recommendations
 import traceback
 from importlib import reload
 from datetime import datetime
-if __name__ == "__main__":
+import json
+import sys
+import subprocess
+def main(delta):
+    print(delta)
     dataset.load('dataset/')
 
-    delta = 123
-    parts = 6
+    
+    parts = 1000
     count = len(dataset.user_books.keys())
     start = int(count / parts) * delta
     end = int(count / parts) * (delta + 1)
@@ -15,7 +19,7 @@ if __name__ == "__main__":
     ct = end - start
     n = 0
 
-    for user in list(dataset.user_books.keys()):
+    for user in list(dataset.user_books.keys())[0:5000]:
         n += 1
         time = datetime.now()
         try:
@@ -28,3 +32,9 @@ if __name__ == "__main__":
             print(f"calculated {n}/{ct} users")
     with open(f"part_{delta}.json", "w") as f:
         f.write(json.dumps(recs))
+
+
+if __name__ == "__main__":
+    main(0)  
+    
+    
